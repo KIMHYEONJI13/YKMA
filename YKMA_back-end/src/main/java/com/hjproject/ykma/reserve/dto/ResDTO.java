79c80b522072462a6889c05a9f4175eedff36425
KIMHYEONJI13 <davithjk1310@gmail.com>
2024-07-04 21:03:45 +0900

    자원예약 class 생성

--- YKMA_back-end/src/main/java/com/hjproject/ykma/reserve/dto/ResDTO.java
+++ YKMA_back-end/src/main/java/com/hjproject/ykma/reserve/dto/ResDTO.java
@@ -1,12 +1,26 @@
 package com.hjproject.ykma.reserve.dto;
 
-public class ResDTO {
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ResDTO {
 
     /**
      * 삼품코드
+     * 상품명
      * 상품정보
-     * 상품
+     * 담당자번호
+     * 상품금액
      * */
+
+    private int resNo;
+    private String resTitle;
+    private String resContent;
+    private int userId;
+    private int price;
 
 }
--- YKMA_back-end/src/main/java/com/hjproject/ykma/schedule/dto/ScheduleDTO.java
+++ YKMA_back-end/src/main/java/com/hjproject/ykma/schedule/dto/ScheduleDTO.java
@@ -1,12 +1,34 @@
 package com.hjproject.ykma.schedule.dto;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ScheduleDTO {
 
     /**
      * 예약번호
-     * 예약자
+     * 예약자번호
      * 예약일시
      * 예약상태
+     * 상품명
      * 담당자
+     * 예약금
      **/
+
+    private int scheduleNo;
+    private int userId;
+    private Date scheduleDttm;
+    private char status;
+    private int managerId;
+
+
+
+
+
 }
